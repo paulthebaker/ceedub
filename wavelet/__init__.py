1471300715a89711c41fead2c996b9e359ec82f8
Paul T. Baker <paultbaker@gmail.com>
2017-09-19 10:18:39 -0400

    import stuff into package namespace

--- wavelet/__init__.py
+++ wavelet/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+# flake8: noqa ignore=F401
 
-"""Top-level package for wavelet."""
+from .wavelet import WaveletBasis
+from .wavelet import MorletWave, PaulWave
 
 __author__ = """Paul T. Baker"""
 __email__ = 'paultbaker@gmail.com'
