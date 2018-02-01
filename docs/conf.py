d8f68698d1f0242b9f656a4601d875ab97e7ebe4
paulthebaker <paultbaker@gmail.com>
2018-02-01 12:41:32 -0500

    use mathjax in docs

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,9 @@ import ceedub
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.mathjax']
+
+mathjax_path = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
