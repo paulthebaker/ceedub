d328b7bcc4d27cdd6578bbbeea0276c2f37edfde
paulthebaker <paultbaker@gmail.com>
2018-02-01 12:43:25 -0500

    rewrite Morlet docstrings

--- ceedub/wavelet.py
+++ ceedub/wavelet.py
@@ -19,16 +19,21 @@ _SQRT2 = np.sqrt(2.)
 
 class MorletWave(object):
     """Morlet-Gabor wavelet: a Gaussian windowed sinusoid
-    w0 is the nondimensional frequency constant.  This defines
+
+    ``w0`` is the nondimensional frequency constant.  This defines
     the base frequency and width of the mother wavelet. For
-    small w0 the wavelets have non-zero mean. T&C set this to
+    small ``w0`` the wavelets have non-zero mean. T&C set ``w0`` to
     6 by default.
-    If w0 is set to less than 5, the modified Morlet wavelet with
-    better low w0 behavior is used instead.
+    If ``w0`` is set to less than 5, the modified Morlet wavelet with
+    better low ``w0`` behavior is used instead.
     """
 
     def __init__(self, w0=6):
-        """initialize Morlet-Gabor wavelet with frequency constant w0
+        """initialize Morlet-Gabor wavelet
+
+        :param w0:
+            Frequency constant defining width and base frequency of
+            mother wavelet.
         """
         self.w0 = w0
         self._MOD = False
@@ -40,21 +45,32 @@ class MorletWave(object):
         return self.time(*args, **kwargs)
 
     def time(self, t, s=1.0):
-        """
+        r"""
         Time domain complex Morlet wavelet, centered at zero.
 
-        :param t: time
-        :param s: scale factor
-        :return psi: value of complex morlet wavelet at time, t
+        :param t:
+            time or list of times to calculate wavelet
+        :param s:
+            scale factor of wavelet (defines which frequency scale)
 
-        The wavelets are defined by dimensionless time: x = t/s
+        :return psi:
+            value of complex morlet wavelet at given times
 
-        For w0 >= 5, computes the standard Morlet wavelet:
-            psi(x) = pi**-0.25 * exp(1j*w0*x) * exp(-0.5*(x**2))
+        The wavelets are defined by dimensionless time: :math:`x = t/s`.
+        For :math:`w_0 \ge 5`, the standard Morlet-Gabor wavelet is used,
+        and for :math`w_0<5`, the modified Morlet-Gabor is used for better
+        stability.
 
-        For w0 < 5, computes the modified Morlet wavelet:
-            psi(x) = pi**-0.25 *
-                        (exp(1j*w0*x) - exp(-0.5*(x**2))) * exp(-0.5*(x**2))
+        .. math:: \psi(x) =
+                \Biggl \lbrace
+                {
+                \pi^{-1/4}\, \exp(i\, w_0\, x)\, \exp(-x^2/2),
+                    \text{ for }{w_0 \ge 5}
+                \atop
+                \pi^{-1/4}\,
+                    \left[\exp(i\, w_0\, x) - \exp(-x^2/2)\right] \exp(-x^2/2),
+                    \text{ for }{w_0 \lt 5}
+                }
         """
         t = np.asarray(t)
         w0 = self.w0
@@ -69,36 +85,72 @@ class MorletWave(object):
         return psi
 
     def fourier_period(self, s):
-        """The Fourier period of the Morlet wavelet with scale, s, given by:
-            P = 4*pi*s / (w0 + sqrt(2 + w0**2))
+        r"""compute Fourier period of the Morlet wavelet
+
+        :param s:
+            scale factor of wavelet
+
+        :return period:
+            period of wavelet
+
+        For a wavelet with scale, :math:`s`, the period is:
+
+        .. math:: P = \frac{4\pi\, s}{w_0 + \sqrt{2 + {w_0}^2}}.
         """
         w0 = self.w0
         return 4*np.pi*s / (w0 + np.sqrt(2 + w0**2))
 
     def nyquist_scale(self, dt=1):
-        """s0 corresponding to the Nyquist period of wavelet
-            s0 = 2*dt * (w0 + sqrt(2 + w0**2)) / (4*pi)
-        for large w0 this is approximately dt*w0/pi
+        r"""compute the scale factor corresponding to the Nyquist period
+        
+        :param dt:
+            sampling time of wavelet basis (time between samples)
+
+        For wavelet basis defined by the mother wavelet with width,
+        :math:`w_0`, and a sampling time, :math:`dt`, the Nyquist period
+        is the smallest period (largest frequency) which can be resolved.
+        For a Morlet wavelet this is
+
+        .. math:: s_0 = 2\,dt\,\frac{w_0 + \sqrt{2 + {w_0}^2}}{4\pi}
+
+        for large :math:`w_0` this is approximately
+        :math:`dt\cdot w_0/\pi`.
         """
         w0 = self.w0
         return dt * (w0 + np.sqrt(2 + w0**2)) / (2*np.pi)
 
     def freq(self, w, s=1.0):
-        """
-        Frequency domain representation of Morlet wavelet
-        Note that the complex Morlet wavelet is real in the frequency domain.
-        :param w: frequency
-        :param s: scale factor
-        :return psi: value of morlet wavelet at frequency, w
+        r"""Frequency domain representation of Morlet wavelet
 
-        Note there is no support for modified Morlet wavelets. The
-        wavelets are defined by dimensionless frequency: y = w*s
+        :param w:
+            frequency or list of frequencies to calculate wavelet
+        :param s:
+            scale factor
 
-        The standard Morlet wavelet is computed as:
-            psi(y) = pi**-.25 * H(y) * exp((-(y-w0)**2) / 2)
+        :return psi:
+            value of morlet wavelet at frequency
 
-        where H(y) is the Heaviside step function:
-            H(y) = (y > 0) ? 1:0
+        The wavelets are defined by dimensionless frequency:
+        :math:`y = w\cdot s`.  The complex Morlet wavelet is real
+        in the frequency domain.  The standard Morlet wavelet is
+        defined in the frequency domain as:
+
+        .. math::
+            \psi(y) = \pi^{-1/4}\, \mathcal{H}(y)\,
+                \exp\left(-\frac{(y-w_0)^2}{2}\right)
+
+        where :math:`\mathcal{H}(y)` is the Heaviside step function:
+
+        .. math::
+            \mathcal{H}(y) =
+                \Biggl \lbrace
+                {
+                1, \text{ for } y \gt 0
+                \atop
+                0, \text{ for } y \le 0
+                }
+
+        There is currently no support for modified Morlet wavelets!
         """
         w = np.asarray(w)
         H = np.zeros_like(w)  # Heaviside array for vector inputs
@@ -109,7 +161,8 @@ class MorletWave(object):
         return H * np.pi**-.25 * np.exp(0.5 * (-(y - w0)**2))
 
     def e_fold(self, s):
-        """The e-folding time for the Morlet wavelet.
+        r"""The e-folding time for the Morlet wavelet.  In dimensionless
+        units of time this is :math:`\sqrt{2}\cdot s`.
         """
         return _SQRT2 * s
 
