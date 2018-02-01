9fdeee8ae0964cda4972f9df615c3a97fa481892
paulthebaker <paultbaker@gmail.com>
2018-02-01 13:27:29 -0500

    rewrite easy.py docstrings

--- ceedub/easy.py
+++ ceedub/easy.py
@@ -9,34 +9,44 @@ from .basis import WaveletBasis
 
 
 def cwt(tdat, dt=1):
-    """Compute the continuous wavelet transform, using the default
-    ``WaveletBasis``.
+    """Compute continuous wavelet transform, using default ``WaveletBasis``.
+
+    :param tdat:
+        shape ``(N,)`` array of real, time domain data
+    :param dt:
+        sample cadence of data, needed for normalization of transforms
+
+    :return wdat: 
+        shape ``(M,N)`` array of complex, wavelet domain data. ``M`` is
+        the number of scales used in the transform, and ``N`` is the
+        length of the input time domain data.
+    
     If you plan on doing several CWTs in the same basis you should
     consider initializing a ``WaveletBasis`` object and using:
-    ``WaveletBasis.cwt``.
-    :param tdat: shape ``(N,)`` array of real, time domain data
-    :param dt: sample cadence of data, needed for normalization
-        of transforms
-    :returns: wdat shape ``(M,N)`` array of complex, wavelet domain data.
-        ``M`` is the number of scales used in the transform, and ``N`` is
-        the length of the input time domain data.
+    ``WaveletBasis.cwt()``.
     """
     WB = WaveletBasis(N=len(tdat), dt=dt)
     return WB.cwt(tdat)
 
 
 def icwt(wdat, dt=1):
-    """Compute the inverse continuous wavelet transform, using the default
+    """Compute inverse continuous wavelet transform, using default
     ``WaveletBasis``.
+
+    :param wdat:
+        shape ``(M,N)`` array of complex, wavelet domain data. ``M`` is
+        the number of frequency scales, and ``N`` is the number of time
+        samples.
+    
+    :return tdat:
+        shape ``(N,)`` array of real, time domain data
+    
     If the forward transform was performed in a different basis, then this
     function will give incorrect output!
+    
     If you plan on doing several ICWTs in the same basis you should seriously
     consider initializing a ``WaveletBasis`` object and using:
-    ``WaveletBasis.cwt`` and ``WaveletBasis.icwt``.
-    :param wdat: shape ``(M,N)`` array of complex, wavelet domain data.
-        ``M`` is the number of frequency scales, and ``N`` is the number of
-        time samples.
-    :returns: tdat shape ``(N,)`` array of real, time domain data
+    ``WaveletBasis.cwt()`` and ``WaveletBasis.icwt()``.
     """
     WB = WaveletBasis(N=wdat.shape[1], dt=dt)
     return WB.icwt(wdat)
@@ -46,9 +56,13 @@ def cwtfreq(N, dt=1):
     """Output the Fourier frequencies of the scales used in the default
     ``WaveletBasis``.
 
-    :param N: number of time samples in the time domain data.
-    :param dt: sample cadence of data
-    returns: shape ``(M,)`` array of frequencies
+    :param N:
+        number of time samples in the time domain data.
+    :param dt:
+        sample cadence of data
+
+    return freqs:
+        shape ``(M,)`` array of frequencies
     """
     WB = WaveletBasis(N=N, dt=dt)
     return WB.freqs
