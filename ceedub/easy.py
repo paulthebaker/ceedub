c84c504217e7340f1e99c13a650e303374e21585
paulthebaker <paultbaker@gmail.com>
2018-02-01 13:19:16 -0500

    use correct name

--- ceedub/easy.py
+++ ceedub/easy.py
@@ -5,7 +5,7 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
-from .transform import WaveletBasis
+from .basis import WaveletBasis
 
 
 def cwt(tdat, dt=1):
